318e90da7310928189c8f144ce3978aba4706a5a
ChristofferHB <christoffer.haarberg@mail.com>
2021-02-14 15:51:56 +0000

    Movie list

--- src/components/MovieFriend/MovieFriend.js
+++ src/components/MovieFriend/MovieFriend.js
@@ -1,28 +1,61 @@
+import React from "react";
 import "./MovieFriend.css";
 import { addMovie, getMovies } from "../../datastore/datastore";
 
-function MovieFriend() {
-  console.log(getMovies());
-  return (
-    <div id="movie-friend-container">
-      <div id="movie-friend-logo">
-        <p>MovieFriend</p>
+class MovieFriend extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movieSearchText: "Search for a movie by name...",
+    };
+    this.movieSearchOnChange = this.movieSearchOnChange.bind(this);
+  }
+
+  movieSearchOnChange(e) {
+    this.setState({
+      movieSearchText: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div id="movie-friend-container">
+        <div id="movie-friend-logo">
+          <p>MovieFriend</p>
+        </div>
+        <div>
+          <form>
+            <label>
+              <input
+                id="movie-friend-search-field"
+                type="text"
+                value={this.state.movieSearchText}
+                name="name"
+                onChange={this.movieSearchOnChange}
+              />
+            </label>
+          </form>
+        </div>
+        <div id="movie-friend-search-content">
+          {getMovies().map((movie, index) => (
+            <div id="movie">
+              <p id="movie-title-year">
+                {movie.title} ({movie.year})
+              </p>
+              <div id="movie-genre-rating">
+                <div>
+                  {" "}
+                  <p id="movie-rating">{movie.rating}</p>
+                </div>
+                <p id="movie-director">{movie.director}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
-        <form>
-          <label>
-            <input
-              id="movie-friend-search-field"
-              type="text"
-              value="Search for movie by name..."
-              name="name"
-            />
-          </label>
-        </form>
-      </div>
-      <div id="movie-friend-search-content"></div>
-    </div>
-  );
+    );
+  }
 }
 
 export default MovieFriend;
